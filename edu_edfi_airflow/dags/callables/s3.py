810995eb64e665523d9b8b0b7eb4f26228eebad5
jkaiser <jayckaiser@gmail.com>
2023-04-04 15:48:12 -0500

    Update local_filepath_to_s3() callable to upload both files and directories.

--- edu_edfi_airflow/dags/callables/s3.py
+++ edu_edfi_airflow/dags/callables/s3.py
@@ -21,20 +21,40 @@ def local_filepath_to_s3(
         s3_hook = S3Hook(aws_conn_id=s3_conn_id)
         s3_bucket = s3_hook.get_connection(s3_conn_id).schema
 
-        s3_hook.load_file(
-            filename=local_filepath,
+        # If a directory, upload all files to S3.
+        if os.path.isdir(local_filepath):
+            for root, dirs, files in os.walk(local_filepath):
+                for file in files:
+                    full_path = os.path.join(root, file)
+                    s3_full_path = os.path.join(s3_destination_key, file)
 
-            bucket_name=s3_bucket,
-            key=s3_destination_key,
+                    s3_hook.load_file(
+                        filename=full_path,
+                        bucket_name=s3_bucket,
+                        key=s3_full_path,
+                        encrypt=True,
+                        replace=True
+                    )
 
-            encrypt=True,
-            replace=True
-        )
+        # Otherwise, upload the single file
+        else:
+            s3_hook.load_file(
+                filename=local_filepath,
+                bucket_name=s3_bucket,
+                key=s3_destination_key,
+                encrypt=True,
+                replace=True
+            )
+
+    # Regardless, delete the local files if specified.
     finally:
         if remove_local_filepath:
             logging.info(f"Removing temporary files written to `{local_filepath}`")
             try:
-                os.remove(local_filepath)
+                if os.path.isdir(local_filepath):
+                    os.rmdir(local_filepath)
+                else:
+                    os.remove(local_filepath)
             except FileNotFoundError:
                 pass
 
