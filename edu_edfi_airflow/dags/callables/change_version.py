716b78e5da5787c22ff4b695415d27c7baa0ca7f
jkaiser <jayckaiser@gmail.com>
2023-05-09 17:05:48 -0500

    Refactor initial change-version callables and bundle into new submodule.

--- edu_edfi_airflow/dags/callables/change_version.py
+++ edu_edfi_airflow/dags/callables/change_version.py
@@ -4,9 +4,29 @@ from typing import Tuple
 from airflow.exceptions import AirflowSkipException, AirflowFailException
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 
-from edu_edfi_airflow.dags.dag_util.airflow_util import get_snowflake_params_from_conn
-from edu_edfi_airflow.dags.dag_util.airflow_util import is_full_refresh, is_resource_specified
+from edu_edfi_airflow.dags.dag_util import airflow_util
+from edu_edfi_airflow.providers.edfi.hooks.edfi import EdFiHook
 
+
+
+def get_newest_edfi_change_version(edfi_conn_id: str, **kwargs):
+    """
+
+    :return:
+    """
+    ### Connect to EdFi ODS and verify EdFi3.
+    edfi_conn = EdFiHook(edfi_conn_id=edfi_conn_id).get_conn()
+
+    # Break off prematurely if change versions not supported.
+    if edfi_conn.is_edfi2():
+        print("Change versions are only supported in EdFi 3+!")
+        return None
+
+    # Pull current max change version from EdFi.
+    max_change_version = edfi_conn.get_newest_change_version()
+    logging.info(f"Current max change version is `{max_change_version}`.")
+
+    return max_change_version
 
 
 def get_resource_change_version(
@@ -40,13 +60,13 @@ def get_resource_change_version(
         If a full refresh is being completed, set all prior change versions in the table to inactive.
     """
     # If doing a resource-specific run, confirm resource is in the list.
-    if not is_resource_specified(kwargs, resource):
+    if not airflow_util.is_resource_specified(kwargs, resource):
         raise AirflowSkipException(
             "Skipping resource not specified in run context 'resources'."
         )
 
     # Retrieve the database and schema from the Snowflake hook.
-    database, schema = get_snowflake_params_from_conn(snowflake_conn_id)
+    database, schema = airflow_util.get_snowflake_params_from_conn(snowflake_conn_id)
 
     # Build the SQL queries to be passed into `Conn.run()`.
     qry_prior_max = f"""
@@ -90,7 +110,7 @@ def get_resource_change_version(
 
     ### Update the change version table if it's a full_refresh run.
     # If marked full_refresh in context, set prior versions as inactive.
-    if full_refresh or is_full_refresh(kwargs):
+    if full_refresh or airflow_util.is_full_refresh(kwargs):
         logging.info(
             "Full refresh, marking previous pulls inactive."
         )
@@ -116,8 +136,103 @@ def get_resource_change_version(
     return edfi_change_version, prior_change_version
 
 
+def reset_change_versions(
+    tenant_code : str,
+    api_year    : int,
 
-def update_change_version_table(
+    *,
+    snowflake_conn_id: str,
+    change_version_table: str,
+
+    full_refresh: bool = False,
+
+    **kwargs
+) -> None:
+    # Airflow-skip if run not marked for a full-refresh.
+    if full_refresh or airflow_util.is_full_refresh(kwargs):
+        logging.info(
+            "Full refresh: marking previous pulls inactive."
+        )
+    else:
+        raise AirflowSkipException(
+            f"Full refresh not specified. Change version table `{change_version_table}` unchanged."
+        )
+
+    ### Prepare the SQL query.
+    # Retrieve the database and schema from the Snowflake hook, and raise an exception if undefined.
+    database, schema = airflow_util.get_snowflake_params_from_conn(snowflake_conn_id)
+
+    # Reset all resources in this tenant-year.
+    qry_mark_inactive = f"""
+        update {database}.{schema}.{change_version_table}
+            set is_active = FALSE
+        where tenant_code = '{tenant_code}'
+        and api_year = {api_year}
+    """
+
+    ### Connect to Snowflake and execute the query.
+    snowflake_conn = SnowflakeHook(snowflake_conn_id).get_conn()
+    with snowflake_conn.cursor() as cur:
+        cur.execute(qry_mark_inactive)
+
+    # No XComs are pushed, so all downstream XCom-pulls will fail and default to 0.
+
+
+def get_previous_change_versions(
+    tenant_code : str,
+    api_year    : int,
+
+    *,
+    snowflake_conn_id: str,
+    change_version_table: str,
+
+    **kwargs
+) -> None:
+    """
+        We separate getting the most recent EdFi change version into a separate function.
+        We need pulls to be consistent by change version for each run.
+        Otherwise, the raw schema will passively drift across resources around versions.
+
+        With every pull from EdFi 3+ to Snowflake, the change-version table is updated for the resource.
+        The change version documents the timestamp of the last pull.
+
+        This operator retrieves the most recent versions found in Snowflake for all resources.
+        Use this and the most recent change version from EdFi to build an ingestion window.
+        At the end of the ingest, the change version table is updated with the most recent version found in EdFi.
+
+        If a full refresh is being completed, all change versions for this tenant-year have been set to inactive.
+    """
+    logging.info(
+        "Retrieving max previously-ingested change versions from Snowflake."
+    )
+
+    ### Prepare the SQL query.
+    # Retrieve the database and schema from the Snowflake hook, and raise an exception if undefined.
+    database, schema = airflow_util.get_snowflake_params_from_conn(snowflake_conn_id)
+
+    # Retrieve the previous max change versions for this tenant-year.
+    qry_prior_max = f"""
+        select name, is_deletes, max(max_version) as max_version
+        from {database}.{schema}.{change_version_table}
+        where tenant_code = '{tenant_code}'
+            and api_year = {api_year}
+            and is_active
+        group by 1, 2
+    """
+
+    ### Connect to Snowflake and execute the query.
+    snowflake_conn = SnowflakeHook(snowflake_conn_id).get_conn()
+    with snowflake_conn.cursor() as cur:
+        cur.execute(qry_prior_max)
+        prior_change_versions = cur.fetchall()
+
+    # Push max change versions as resource-level XComs.
+    for snake_resource, is_deletes, max_version in prior_change_versions:
+        xcom_key = airflow_util.build_display_name(snake_resource, is_deletes)
+        kwargs['ti'].xcom_push(key=xcom_key, value=max_version)
+
+
+def update_resource_change_version(
     tenant_code: str,
     api_year   : int,
     resource   : str,
@@ -128,14 +243,14 @@ def update_change_version_table(
 
     edfi_change_version: int,
 
-    **kwargs,
+    **kwargs
 ):
     """
 
     :return:
     """
     # Retrieve the database and schema from the Snowflake hook.
-    database, schema = get_snowflake_params_from_conn(snowflake_conn_id)
+    database, schema = airflow_util.get_snowflake_params_from_conn(snowflake_conn_id)
 
     # Build the SQL queries to be passed into `Hook.run()`.
     qry_insert_into = f"""
