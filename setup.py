7864b972319cc5d2857170d0883bd144c7b7113d
jkaiser <jayckaiser@gmail.com>
2023-08-16 17:34:24 -0500

    Update setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
       name='edu_edfi_airflow',
-      version='0.1.2',
+      version='0.2.0',
       description='EDU Airflow tools for Ed-Fi',
       license_files=['LICENSE.md'],
       url='https://github.com/edanalytics/edu_edfi_airflow',
