5c9ed0230389d32e967e3c6b166f3690bd9e2dcc
johncmerfeld <John.Merfeld@gmail.com>
2024-11-12 14:28:16 -0600

    safe map cast

--- edu_edfi_airflow/dags/edfi_resource_dag.py
+++ edu_edfi_airflow/dags/edfi_resource_dag.py
@@ -453,7 +453,7 @@ class EdFiResourceDAG:
         Many XComs in this DAG are lists of tuples. This overloads xcom_pull_template to retrieve a list of items at a given index.
         """
         return airflow_util.xcom_pull_template(
-            task_ids, suffix=f" or [] | map(attribute={idx}) | list"
+            task_ids, suffix=f" | map(attribute={idx}) or [] | list"
         )
     
     @staticmethod
