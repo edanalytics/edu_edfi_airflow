06e2bee8c01b06d92b4282b661907c6275a24c2e
jayckaiser <jayckaiser@gmail.com>
2025-03-24 17:44:10 -0500

    Turn off EM exit code logic while testing student ID bundle updates.

--- edu_edfi_airflow/dags/earthbeam_dag.py
+++ edu_edfi_airflow/dags/earthbeam_dag.py
@@ -901,12 +901,13 @@ class EarthbeamDAG:
                     state_file=em_state_file,
                     snowflake_read_conn_id=snowflake_read_conn_id,
                     results_file=em_results_file,
-                    return_exit_code=True,
+                    # return_exit_code=True,
                     **self.inject_parameters_into_kwargs(env_mapping, earthmover_kwargs),
                     dag=self.dag
                 )
 
-                data_dir, exit_code = earthmover_operator.execute(**context)
+                # data_dir, exit_code = earthmover_operator.execute(**context)
+                data_dir, exit_code = earthmover_operator.execute(**context), None
 
                 if exit_code:
                     context['ti'].xcom_push(key="data_dir", value=data_dir)
