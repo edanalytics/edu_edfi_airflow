a836b0ae94c20e2db46d73819fe2b500c940f002
jayckaiser <jayckaiser@gmail.com>
2025-06-05 15:34:32 -0500

    Revert EarthmoverOperator and apply logging wrapper to operator in EarthbeamDAG.

--- edu_edfi_airflow/providers/earthbeam/operators.py
+++ edu_edfi_airflow/providers/earthbeam/operators.py
@@ -7,7 +7,6 @@ from airflow.models import Connection
 from airflow.operators.bash import BashOperator
 
 from edu_edfi_airflow.callables import airflow_util
-from edu_edfi_airflow.callables.log_util import capture_logs_to_snowflake
 
 
 class EarthmoverOperator(BashOperator):
@@ -28,14 +27,6 @@ class EarthmoverOperator(BashOperator):
         results_file: Optional[str] = None,
         snowflake_read_conn_id: Optional[str] = None,
 
-        # Logging args
-        snowflake_log_conn_id: Optional[str] = None,
-        logging_table: Optional[str] = None,
-        tenant_code: Optional[str] = None,
-        api_year: Optional[str] = None,
-        grain_update: Optional[str] = None,
-        run_type: Optional[str] = None,
-
         force          : bool = False,
         skip_hashing   : bool = False,
         show_graph     : bool = False,
@@ -47,13 +38,6 @@ class EarthmoverOperator(BashOperator):
         self.output_dir = output_dir
         self.state_file = state_file
         self.snowflake_read_conn_id = snowflake_read_conn_id
-
-        self.snowflake_log_conn_id = snowflake_log_conn_id
-        self.logging_table = logging_table
-        self.tenant_code = tenant_code
-        self.api_year = api_year
-        self.grain_update = grain_update
-        self.run_type = run_type
 
         ### Building the Earthmover CLI command
         self.arguments = {}
@@ -120,23 +104,12 @@ class EarthmoverOperator(BashOperator):
         
         self.bash_command += " ".join(cli_arguments)
         logging.info(f"Complete Earthmover CLI command: {self.bash_command}")
-        logging.info(f"Snowflake Log Conn ID: {self.snowflake_log_conn_id}")
-        logging.info(f"Snowflake Logging Table: {self.logging_table}")
 
         # Create state_dir if not already defined in filespace
         os.makedirs(os.path.dirname(self.state_file), exist_ok=True)
 
-        wrapped_execute = capture_logs_to_snowflake(
-            super().execute,
-            snowflake_conn_id=self.snowflake_log_conn_id,
-            logging_table=self.logging_table,
-            tenant_code=self.tenant_code,
-            api_year=self.api_year,
-            grain_update=self.grain_update,
-            run_type=self.run_type,
-        )
-
-        return wrapped_execute(context=context)
+        super().execute(context)
+        return self.output_dir
 
 
 
