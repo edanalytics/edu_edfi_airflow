4a4d097159f77c57dfd639f5e763728b51dfe7bb
Jacob Bortell <jbortell@edanalytics.org>
2026-05-28 07:01:54 -0500

    Add is_sideloaded to the select part of the statement

--- edu_edfi_airflow/interfaces/database.py
+++ edu_edfi_airflow/interfaces/database.py
@@ -155,7 +155,7 @@ class SnowflakeDatabaseInterface(DatabaseInterface):
 
         qry = f"""
             COPY INTO {self.database}.{self.schema}.{table}
-                (tenant_code, api_year, pull_date, pull_timestamp, file_row_number, filename, name, ods_version, data_model_version, v)
+                (tenant_code, api_year, pull_date, pull_timestamp, file_row_number, filename, name, ods_version, data_model_version, is_sideloaded, v)
             FROM (
                 SELECT
                     '{tenant_code}' AS tenant_code,
@@ -191,7 +191,7 @@ class SnowflakeDatabaseInterface(DatabaseInterface):
 
         qry = f"""
             COPY INTO {self.database}.{self.schema}.{table}
-                (tenant_code, api_year, pull_date, pull_timestamp, file_row_number, filename, name, ods_version, data_model_version, v)
+                (tenant_code, api_year, pull_date, pull_timestamp, file_row_number, filename, name, ods_version, data_model_version, is_sideloaded, v)
             FROM (
                 SELECT
                     '{tenant_code}' AS tenant_code,
